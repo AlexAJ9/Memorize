e5614aa6e909132c18af22448c560c3c41382bb1
alexanderAj99 <alexandar.aldzhbari@quanterall.com>
2024-03-02 19:06:54 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Alexander A on 2.03.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
